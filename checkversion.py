9462c56d6e0f05cfda6ef7cac7d64467e4a0339c
Elija-K <elija.kaeser@proton.me>
2024-04-13 22:43:03 +0200

    check version json

---
+++ checkversion.py
@@ -0,0 +1,6 @@
+# The Variables for everything
+filesserverside = "\\\\ODW-Master-01\public\odwLauncher_games"
+fileslocalside = "~\odwlauncher\games"
+# Checking the Server JSON Files
+
+# Checking the Local JSON Files
